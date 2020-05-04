5007483ea06f41e0920f7442b239983b5a5d963e
Jose Maria Nieva Toppa <cpn.nievatoppa@gmail.com>
2020-05-04 08:37:39 -0300

    creacion backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "./img/logo.jpg";
 //import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 export default function App() {
   return (
     <Router>
-      <Navbar bg="dark" variant="dark" expand="md" className=''>
+      <Navbar bg="dark" variant="dark" expand="md" >
         <Navbar.Brand href="#home">
-           
-            <Link to="/">
-            <img src={logo} alt="" className='w-25' />
-            </Link>
+
+          <Link to="/">
+            <img src={logo} alt="" width="50" height="50" />
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto align-items-center">
+        <Navbar.Collapse id="basic-navbar-nav justify-content-end">
+          <Nav className="mr-auto align-items-center justify-content-end">
             <Link to="/articulos" className='text-wrap p-2 text-center  text-white'>Articulos</Link>
             <Link to="/links" className='text-wrap p-2 text-center  text-white'>Seguir Aprendiendo</Link>
             <Link to="/concursos" className='text-wrap p-2 text-center  text-white'>Concursos</Link>
@@ -43,7 +44,7 @@ export default function App() {
         </Navbar.Collapse>
       </Navbar>
       <div>
-               {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/articulos">
@@ -72,6 +73,45 @@ export default function App() {
           </Route>
         </Switch>
       </div>
+      <Container>
+
+      <footer className="pt-4 my-md-5 pt-md-5 border-top">
+        <div className="d-flex flex-column align-items-center justify-content-center">
+                     <img className="mb-2" src={logo} alt="" width="50" height="50"/>
+              <small className="d-block mb-3 text-muted text-wrap">Aprender A Emprender</small>
+            <small className="d-block mb-3 text-muted text-wrap">2020</small>
+                   {/* <div className="col-6 col-md">
+              <h5>Features</h5>
+              <ul className="list-unstyled text-small">
+                <li><a className="text-muted" href="#">Cool stuff</a></li>
+                <li><a className="text-muted" href="#">Random feature</a></li>
+                <li><a className="text-muted" href="#">Team feature</a></li>
+                <li><a className="text-muted" href="#">Stuff for developers</a></li>
+                <li><a className="text-muted" href="#">Another one</a></li>
+                <li><a className="text-muted" href="#">Last time</a></li>
+              </ul>
+            </div>
+            <div className="col-6 col-md">
+              <h5>Resources</h5>
+              <ul className="list-unstyled text-small">
+                <li><a className="text-muted" href="#">Resource</a></li>
+                <li><a className="text-muted" href="#">Resource name</a></li>
+                <li><a className="text-muted" href="#">Another resource</a></li>
+                <li><a className="text-muted" href="#">Final resource</a></li>
+              </ul>
+            </div>
+            <div className="col-6 col-md">
+              <h5>About</h5>
+              <ul className="list-unstyled text-small">
+                <li><a className="text-muted" href="#">Team</a></li>
+                <li><a className="text-muted" href="#">Locations</a></li>
+                <li><a className="text-muted" href="#">Privacy</a></li>
+                <li><a className="text-muted" href="#">Terms</a></li>
+              </ul>
+            </div> */}
+            </div>
+  </footer>
+      </Container>
     </Router>
   );
 }
