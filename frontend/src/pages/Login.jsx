986db90a0df2f834b7b9ce4122a413293ab153d8
Jose Maria Nieva Toppa <cpn.nievatoppa@gmail.com>
2020-05-29 11:57:39 -0300

    fix login authentification

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,8 +6,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import desktop from "../img/desktop.jpg";
 import {
-  useHistory
-  } from "react-router-dom";
+    useHistory
+} from "react-router-dom";
 
 
 export default function Login() {
@@ -18,33 +18,41 @@ export default function Login() {
 
     useEffect(() => {
         const fetchUsers = async () => {
-          try {
-            const response = await axios.get("http://localhost:3000/users/login");
-            console.log(response.data);
-            setUsers(response.data);
-          } catch (error) {
-            console.error(error);
-          }
+            try {
+                const response = await axios.get("http://localhost:3000/users/login");
+                console.log(response.data);
+                setUsers(response.data);
+            } catch (error) {
+                console.error(error);
+            }
         };
         fetchUsers();
-      }, []);
+    }, []);
 
-      const handleChangeEmail = e => setEmail(e.target.value);
-      const handleChangeContraseña = e => setContraseña(e.target.value);
-    
+    const handleChangeEmail = e => setEmail(e.target.value);
+    const handleChangeContraseña = e => setContraseña(e.target.value);
+
     const handleClick = () => {
-        localStorage.setItem("logged", JSON.stringify(true));
-        localStorage.setItem("tipoUsuario", JSON.stringify("alumno"));
-        history.push("/cuenta")
+        const usuario = users.find(usuario => usuario.email === email);
+        console.log(usuario);
+        if (usuario) {
+            if (usuario.contraseña === contraseña) {
+                localStorage.setItem("logged", JSON.stringify(true));
+                localStorage.setItem("tipoUsuario", JSON.stringify("alumno"));
+                history.push("/cuenta")
+            } else { }
+        } else { history.push("/") }
     }
     return (
-        <div className="d-flex justify-content-center h-100 p-5" style={{backgroundImage: `url(${desktop})`,
-        backgroundPosition: "center center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        width: "100%",
-        height: "auto",
-        margin: "auto"}}>
+        <div className="d-flex justify-content-center h-100 p-5" style={{
+            backgroundImage: `url(${desktop})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            width: "100%",
+            height: "auto",
+            margin: "auto"
+        }}>
             <Card className="w-80 p-5" style={{ backgroundImage: "linear-gradient(#53c68c,#133926)" }}>
                 <Card.Header>
                     <h3>Ingresa A Tu Cuenta</h3>
@@ -60,7 +68,7 @@ export default function Login() {
                         <Form.Group controlId="formBasicCheckbox">
                             <Form.Check className="text-white" type="checkbox" label="Recordarme" />
                         </Form.Group>
-                        <Button onClick={handleClick} className="text-black" type="submit" style={{backgroundColor: "#66cc99"}}>
+                        <Button onClick={handleClick} className="text-black" type="submit" style={{ backgroundColor: "#66cc99" }}>
                             Ingresar
   </Button>
                     </Form>
